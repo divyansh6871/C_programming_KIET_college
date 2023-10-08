d43a7cc818ae49db1f1bb94a21ba168bcfe7988e
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2023-10-08 17:59:35 +0530

    Update Even_odd.c

--- Even_odd.c
+++ Even_odd.c
@@ -1,1 +1,15 @@
 
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter the number and I would tell you that wether it is even or odd : ");
+    scanf("%d",&n);
+    if(n%2==0)
+    printf("The number you entered is EVEN!");
+    else
+    printf("THe number you entered is ODD!");
+
+    return 0;
+}
