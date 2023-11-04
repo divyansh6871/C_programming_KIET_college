600ba470a03418a608fd294169d7c1b2b36340c8
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2023-11-04 22:09:01 +0530

    Add files via upload

---
+++ G.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+void alphabet(int height)
+{
+    for (int row = 0; row <= height; row++)
+    {
+        for (int col = 0; col < height ; col++)
+        {
+            if ((row==0&&col!=0)||(col==0&&row!=0&&row<=height-3)||(row==height-2&&col<height/2-1&&col!=0)||col==height/2-1&&(row>=height/2&&row<=height-2)||(row==height/2&&col>=height/2)||(col==height-1&&row>height/2))
+            {
+                printf("* ");
+            }
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    alphabet(8);
+    return 0;
+}
