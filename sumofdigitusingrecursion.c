7d43732783cf91d55ceb1468558d7bdae3750dd6
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2023-11-22 23:02:43 +0530

    Update sumofdigitusingrecursion.c

--- sumofdigitusingrecursion.c
+++ sumofdigitusingrecursion.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h>
+int sum(int n,int total){
+    if(n>0){
+        total=total+n%10;
+        n=n/10;
+        if(n==0)
+            return total;
+        sum(n,total);
+    }
+    else
+        return 0;
+}
+int main()
+{
+    printf("%d",sum(167,0));
+    return 0;
+}
