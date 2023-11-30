01b1363977ffbeb1768f1a4f0f5b0c95918ee2f6
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2023-12-01 01:06:49 +0530

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int fibo(int n)
+{
+    if (n == 0 || n == 1)
+        return n;
+    else
+        return (fibo(n - 1) + fibo(n - 2));
+}
+int main()
+{
+    int n = 7;
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", fibo(i));
+    }
+    printf("\n%d",fibo(n-1));
 
+    return 0;
+}
