aecd764e29eb54857ad7309aaa0f24171cb57367
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2023-12-01 00:59:31 +0530

    Update fact_by_tail_recu.c

--- fact_by_tail_recu.c
+++ fact_by_tail_recu.c
@@ -1,1 +1,18 @@
-
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int fact(int n,int res){
+    if (n==0||n==1)
+    {
+        return res;
+    }
+    else{
+        return fact(n-1,n*res);
+    }
+    
+}
+int main()
+{
+    printf("%d",fact(7,1));
+    return 0;
+}
