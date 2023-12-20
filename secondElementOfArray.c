28df6cacd1eee496d5a31dd9f535502388538dca
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2023-12-20 23:22:40 +0530

    Update secondElementOfArray.c

--- secondElementOfArray.c
+++ secondElementOfArray.c
@@ -1,14 +1,36 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include <stdio.h>
 int main()
 {
-    int arr[5];
-    for (int i=0;i<5;i++)
+    int n;
+    printf("Enter the number : ");
+    scanf("%d", &n);
+    if (n > 2)
     {
-        scanf("%d",&arr[i]);
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            printf("Enter array element : ");
+            scanf("%d", &arr[i]);
+        }
+        int highest = arr[0];
+        for (int i = 1; i < n; i++)
+        {
+            if (highest < arr[i])
+            {
+                highest = arr[i];
+            }
+        }
+        printf("Highest element is %d.\n", highest);
+        int p = arr[0];
+        for (int i = 1; i < n; i++)
+        {
+            if (p < highest && p < arr[i] && (arr[i] != highest))
+            {
+                p = arr[i];
+            }
+        }
+        printf("Second highest elemet is : %d.", p);
     }
-    
-     
+
     return 0;
 }
